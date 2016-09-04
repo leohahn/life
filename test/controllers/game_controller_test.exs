dbce3f5f0b4411ae5554c91ac90b4d3d26d14521
Leonardo Hahn <lhahn@lhahn.fritz.box>
2016-09-04 20:13:24 +0200

    Finished game_controller.ex and its tests.

--- test/controllers/game_controller_test.exs
+++ test/controllers/game_controller_test.exs
@@ -15,11 +15,43 @@ defmodule Life.GameControllerTest do
   end
 
   test "user needs to be authenticated", %{conn: conn} do
-    conn = get conn, game_path(conn, :index)
-    assert json_response(conn, 401) == render_error_json("401.json")
+    conn1 = get conn, game_path(conn, :index)
+    assert json_response(conn1, 401) == render_error_json("401.json")
+
+    conn2 = get conn, game_path(conn, :show, 1)
+    assert json_response(conn2, 401) == render_error_json("401.json")
   end
 
-  defp render_json(template, assigns \\ %{}) do
+  @tag login_as: "max"
+  test "user cann see all his games", %{conn: conn, user: user} do
+    other_user = insert(:user, username: "otherguy")
+
+    max_game1 = insert(:game, name: "Game 1", user: user)
+    max_game2 = insert(:game, name: "Game 2", user: user)
+    _ = insert(:game, name: "Not Max's game", user: other_user)
+
+    new_conn = get conn, game_path(conn, :index)
+    assert json_response(new_conn, 200) ==
+      render_json("index.json", games: [max_game2, max_game1])
+  end
+
+  @tag login_as: "max"
+  test "user can see a specific game of his", %{conn: conn, user: user} do
+    other_user = insert(:user, username: "otherguy")
+
+    max_game = insert(:game, name: "Game 1", user: user)
+    not_max_game = insert(:game, name: "Not Max's game", user: other_user)
+
+    new_conn = get conn, game_path(conn, :show, not_max_game.id)
+    assert json_response(new_conn, 404) ==
+      render_error_json("404.json")
+
+    new_conn = get conn, game_path(conn, :show, max_game.id)
+    assert json_response(new_conn, 200) ==
+      render_json("show.json", game: max_game)
+  end
+
+  defp render_json(template, assigns) do
     Life.GameView.render(template, assigns)
     |> Poison.encode!
     |> Poison.decode!
--- web/controllers/game_controller.ex
+++ web/controllers/game_controller.ex
@@ -4,26 +4,40 @@ defmodule Life.GameController do
   plug Guardian.Plug.EnsureAuthenticated, handler: __MODULE__
 
   def index(conn, _params, current_user) do
-    games = Repo.all(Life.Game)
+    games =
+      current_user
+      |> query_user_games
+      |> Repo.all()
     render(conn, "index.json", games: games)
   end
 
   def show(conn, %{"id" => id}, current_user) do
-    case Repo.get(Life.Game, id) do
+    case Repo.get(query_user_games(current_user), id) do
+      nil ->
+        conn
+        |> put_status(404)
+        |> render(Life.ErrorView, "404.json", %{})
+
       game ->
         render(conn, "show.json", game: game)
-
-      nil ->
-        render(conn, "404.json")
     end
   end
 
+  # Query that gets all games from a specific user.
+  defp query_user_games(user) do
+    from g in Life.Game, where: g.user_id == ^user.id
+  end
+
+  # This function is called whenever the user is not on the
+  # authorization header. It always return a 401 status.
   def unauthenticated(conn, _params) do
     conn
     |> put_status(401)
     |> render(Life.ErrorView, "401.json", %{})
   end
 
+  # Overwrites the action function, so that it always passes the
+  # current user as the last parameter.
   def action(conn, _) do
     user = Guardian.Plug.current_resource(conn)
     args = [conn, conn.params, user]
